67097cb366defc16ebf06d118e26a247f6707479
Einar Persson <me@einarpersson.com>
2024-12-22 19:23:28 +0100

    make sure zsh integration starts a session to trigger envsubst hook

--- scripts/shell-integration/integration.zsh
+++ scripts/shell-integration/integration.zsh
@@ -3,7 +3,7 @@ _aichat_zsh() {
         local _old=$BUFFER
         BUFFER+="⌛"
         zle -I && zle redisplay
-        BUFFER=$(aichat -e "$_old")
+        BUFFER=$(aichat -s shell --empty-session -e "$_old")
         zle end-of-line
     fi
 }
